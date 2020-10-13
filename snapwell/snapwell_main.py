ef11d1d4d393eb865b449284eedf6451b669a010
Eivind Jahren <eivind.jahren@webstep.no>
2020-10-13 15:56:40 +0200

    Refactor snapwell_main

--- snapwell/snapwell_main.py
+++ snapwell/snapwell_main.py
@@ -14,277 +14,308 @@
 #  See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html>
 #  for more details.
 
-from __future__ import print_function  # so we can write print(..,end='')
 
 import argparse
+import logging
 import sys
+import warnings
 from os import makedirs, path
 from time import time
 
+from snapwell import __version__ as VERSION
+from snapwell import snap, tryFloat
 from snapwell.snapconfig import SnapConfig
-from snapwell import __version__ as VERSION, snap, tryFloat
 
 
-def exit_with_usage(msg=None, exit_status=0):
-    if msg:
-        print(msg)
-    print("usage:          snapwell conf.sc")
-    print("                snapwell -h")
-    print("                snapwell --help")
-    print("                snapwell --version")
-    print("")
-    print("Visualization:")
-    print("                snapviz conf.sc")
-    exit(exit_status)
+def warn_without_traceback(message, category, filename, lineno, file=None, line=None):
+
+    log = file if hasattr(file, "write") else sys.stderr
+    log.write(f"Warning: {message}\n")
 
 
-def get_conf_file(conf_file):
-    if not path.exists(conf_file):
-        exit_with_usage("No such file or directory: %s" % conf_file, 1)
-
-    if not path.isfile(conf_file):
-        exit_with_usage(
-            "A Snapwell config file is needed.  Provide full path to config file."
-        )
-
-    if not conf_file[-3:] == ".sc":
-        print(
-            "Warning: It is highly recommended that a Snapwell config file has file extension .sc"
-        )
-    return conf_file
+class ParserPrintHelp(argparse.ArgumentParser):
+    def error(self, message, exit_code=2):
+        sys.stderr.write("error: %s\n" % message)
+        self.print_help()
+        sys.exit(exit_code)
 
 
-def get_snap_conf(conf_file):
-    print("Parsing config file %s" % conf_file)
-    try:
-        return SnapConfig.parse(conf_file)
-    except Exception as err:
-        exit_with_usage(str(err), 1)
+class SnapwellRunner:
+    def __init__(self, config, grid, restart, init, wellpaths, resinsight):
+        self.config = config
+        self.grid = grid
+        self.restart = restart
+        self.init = init
+        self.wellpaths = wellpaths
+        self.resinsight = resinsight
 
-
-def load_grid_file(snap_conf):
-    print("Loading grid %s" % snap_conf.gridFile())
-    grid = None
-    gridFile = snap_conf.gridFile()
-    try:
-        grid = snap_conf.getGrid()
-    except Exception as err:
-        print("Warning, supplied GRID file not loaded: %s" % err)
-    if not grid:
-        if not path.isfile(gridFile):
-            exit_with_usage("Missing grid: No such file %s." % gridFile)
-        else:
-            exit_with_usage("Missing grid: Failed to read grid file %s." % gridFile)
-    return grid
-
-
-def load_restart_file(snap_conf):
-    print("Loading restart %s" % snap_conf.restartFile())
-    restart = None
-    restartFile = snap_conf.restartFile()
-    try:
-        restart = snap_conf.getRestart()
-    except Exception as err:
-        print("Warning, supplied RESTART file not loaded: %s" % err)
-
-    if not restart:
-        if not path.isfile(restartFile):
-            exit_with_usage("Missing restart: No such file %s." % restartFile)
-        else:
-            exit_with_usage(
-                "Missing restart: Failed to read restart file %s." % restartFile
-            )
-    return restart
-
-
-def load_init_file(snap_conf):
-    init = None
-    if snap_conf.initFile():
-        print("Loading INIT %s" % snap_conf.initFile())
+    def run_and_write(self, wp, wp_date):
         try:
-            init = snap_conf.getInit()
+            # call to main algorithm
+            snap(
+                wp,
+                self.grid,
+                self.restart,
+                self.init,
+                wp_date,
+                owc_offset=self.config.owcOffset(),
+                keywords=self.config.logKeywords(),
+                delta=self.config.deltaZ(),
+                owc_definition=self.config.owcDefinition(),
+            )
+            # write wellpath to file
+            rows = wp.write(
+                overwrite=self.config.overwrite(), resinsight=self.resinsight
+            )
+            print("Wrote %d rows to %s.out" % (rows, wp.filename()))
+            # done with this wellpath
+        except ValueError as err:
+            print("Error in well/grid/restart values: %s" % err)
+        except IOError as err:
+            print("Error while writing file: %s" % err)
+
+    def main_loop(self):
+        num_snaps = len(self.wellpaths)
+        print("delta_z    = %.3f" % self.config.deltaZ())
+        print("owc_offset = %.3f" % self.config.owcOffset())
+        owc_def = self.config.owcDefinition()
+        print("owc_defini = %.3f (%s)" % (owc_def[1], owc_def[0]))
+        print("output     = %s" % self.config.output())
+        for i, wp in enumerate(self.wellpaths):
+            wp_date = self.config.date(i)
+            sep = "=" * 79
+            print("\n\n%s" % sep)
+            print("%d/%d \t Snapping %s" % (i + 1, num_snaps, wp.wellname()))
+            start = time()
+            self.run_and_write(wp, wp_date)
+            stop = time()
+            sec = round(stop - start, 2)
+            print("Operation took %s seconds" % str(sec))
+
+
+class SnapwellApp:
+    def __init__(self, argv):
+        self.parser = self.make_parser(argv[0])
+        self.argv = argv[1:]
+
+    def parse_args(self):
+        return self.parser.parse_args(self.argv)
+
+    def load_config(self, args):
+        snap_conf = args.config
+
+        if args.owc_offset:
+            snap_conf.setOwcOffset(args.owc_offset)
+        if args.output:
+            outpath = path.abspath(args.output)
+            if path.isfile(outpath):
+                self.exit_with_usage(
+                    "Output path is an existing file.  Delete file, or choose a different output path."
+                )
+            if not path.exists(outpath):
+                makedirs(outpath)
+            snap_conf.setOutput(outpath)
+        if args.overwrite:
+            snap_conf.setOverwrite(args.overwrite)
+        if args.owc_definition:
+            snap_conf.setOwcDefinition(args.owc_definition)
+        if args.owc_offset:
+            snap_conf.setOwcOffset(args.owc_offset)
+        if args.delta:
+            snap_conf.setDeltaZ(args.delta)
+        return snap_conf
+
+    def load_wellpaths(self, config):
+        print("Loading %d wells" % (len(config)))
+
+        wellpaths = []
+        for i in range(len(config)):
+            fname = config.filename(i)
+            print(fname, end=" ... ")
+            wp = config.getWellpath(i)
+            print("(%d points, %d logs)" % (len(wp), len(wp.headers())), end=" ")
+            wellpaths.append(wp)
+            print("done")
+        return wellpaths
+
+    def load_restart_file(self, config):
+        print("Loading restart %s" % config.restartFile())
+        restart = None
+        restartFile = config.restartFile()
+        try:
+            restart = config.getRestart()
         except Exception as err:
-            print("Warning, supplied INIT file not loaded: %s" % err)
+            warnings.warn(f"supplied RESTART file not loaded: {err}")
 
-    if not init:
-        print("No INIT file, will not output PERM values")
-    return init
+        if not restart:
+            if not path.isfile(restartFile):
+                self.exit_with_usage("Missing restart: No such file %s." % restartFile)
+            else:
+                self.exit_with_usage(
+                    "Missing restart: Failed to read restart file %s." % restartFile
+                )
+        return restart
 
+    def load_grid_file(self, config):
+        print("Loading grid %s" % config.gridFile())
+        grid = None
+        gridFile = config.gridFile()
+        try:
+            grid = config.getGrid()
+        except Exception as err:
+            warnings.warn("supplied GRID file not loaded: %s" % err)
+        if not grid:
+            if not path.isfile(gridFile):
+                self.exit_with_usage("Missing grid: No such file %s." % gridFile)
+            else:
+                self.exit_with_usage(
+                    "Missing grid: Failed to read grid file %s." % gridFile
+                )
+        return grid
 
-def load_wellpaths(snap_conf):
-    print("Loading %d wells" % (len(snap_conf)))
+    def load_init_file(self, config):
+        init = None
+        if config.initFile():
+            print("Loading INIT %s" % config.initFile())
+            try:
+                init = config.getInit()
+            except Exception as err:
+                warnings.warn(f"supplied INIT file not loaded: {err}")
 
-    wellpaths = []
-    for i in range(len(snap_conf)):
-        fname = snap_conf.filename(i)
-        print(fname, end=" ... ")
-        wp = snap_conf.getWellpath(i)
-        print("(%d points, %d logs)" % (len(wp), len(wp.headers())), end=" ")
-        wellpaths.append(wp)
-        print("done")
-    return wellpaths
+        if not init:
+            print("No INIT file, will not output PERM values")
+        return init
 
+    def exit_with_usage(self, msg=None, exit_code=0):
+        self.parser.error(message=msg, exit_code=exit_code)
 
-def run_and_write(grid, restart, init, snap_conf, wp, wp_date, resinsight=False):
-    try:
-        # call to main algorithm
-        snap(
-            wp,
-            grid,
-            restart,
-            init,
-            wp_date,
-            owc_offset=snap_conf.owcOffset(),
-            keywords=snap_conf.logKeywords(),
-            delta=snap_conf.deltaZ(),
-            owc_definition=snap_conf.owcDefinition(),
+    @staticmethod
+    def make_parser(prog):
+        def snapwell_config_file(conf_file):
+            if not path.exists(conf_file):
+                raise argparse.ArgumentTypeError(
+                    f"No such file or directory: {conf_file}"
+                )
+
+            if not path.isfile(conf_file):
+                raise argparse.ArgumentTypeError(
+                    "A Snapwell config file is needed.  Provide full path to config file."
+                )
+
+            if not conf_file[-3:] == ".sc":
+                warnings.warn(
+                    "It is highly recommended that a Snapwell config file has file extension .sc"
+                )
+            print("Parsing config file %s" % conf_file)
+            try:
+                return SnapConfig.parse(conf_file)
+            except ValueError as e:
+                raise argparse.ArgumentTypeError(
+                    f"Error while parsing snapwell config file: {e}"
+                ) from e
+
+        def owc_definition(od):
+            od = od.strip()
+            if ":" not in od:
+                raise argparse.ArgumentTypeError(
+                    "owc definition is malformed: missing ':'"
+                )
+            t = od.split(":")
+            if len(t) != 2:
+                raise argparse.ArgumentTypeError(
+                    "owc definition is malformed: more than one ':'"
+                )
+            odkw, odval = t[0], t[1]
+            odval = tryFloat(odval, ret=None)
+            if odval is None:
+                raise argparse.ArgumentTypeError(
+                    f"owc definition is malformed: could not parse value {odval}"
+                )
+            if not odkw.isalpha():
+                raise argparse.ArgumentTypeError(
+                    f"owc definition is malform: could not parse key word {odkw}"
+                )
+            return odkw.upper(), odval
+
+        parser = ParserPrintHelp(
+            prog=prog, description="Snapwell --- a wellpath optimization program."
         )
-        # write wellpath to file
-        rows = wp.write(overwrite=snap_conf.overwrite(), resinsight=resinsight)
-        print("Wrote %d rows to %s.out" % (rows, wp.filename()))
-        # done with this wellpath
-    except ValueError as err:
-        print("Error in well/grid/restart values: %s" % err)
-    except IOError as err:
-        print("Error while writing file: %s" % err)
-
-
-def _parseOwcDefinition(od):
-    od = od.strip()
-    if not ":" in od:
-        return None
-    t = od.split(":")
-    if len(t) != 2:
-        return None
-    odkw, odval = t[0], t[1]
-    odval = tryFloat(odval, ret=None)
-    if odval is None:
-        return None
-    if not odkw.isalpha():
-        return None
-    return odkw.upper(), odval
-
-
-def main_loop(grid, restart, init, snap_conf, wellpaths, resinsight=False):
-    num_snaps = len(wellpaths)
-    print("delta_z    = %.3f" % snap_conf.deltaZ())
-    print("owc_offset = %.3f" % snap_conf.owcOffset())
-    owc_def = snap_conf.owcDefinition()
-    print("owc_defini = %.3f (%s)" % (owc_def[1], owc_def[0]))
-    print("output     = %s" % snap_conf.output())
-    for i in range(len(wellpaths)):
-        wp = wellpaths[i]
-        wp_date = snap_conf.date(i)
-        sep = "=" * 79
-        print("\n\n%s" % sep)
-        print("%d/%d \t Snapping %s" % (i + 1, num_snaps, wp.wellname()))
-        start = time()
-        run_and_write(
-            grid, restart, init, snap_conf, wp, wp_date, resinsight=resinsight
+        parser.add_argument(
+            "-o",
+            "--output",
+            type=str,
+            help="Output folder.  In this folder, all the new wellpath files are written on the specified format.",
         )
-        stop = time()
-        sec = round(stop - start, 2)
-        print("Operation took %s seconds" % str(sec))
+        parser.add_argument(
+            "-z", "--owc_offset", type=float, help="OWC offset in meters, e.g. 0.5"
+        )
+        parser.add_argument(
+            "-f",
+            "--owc_definition",
+            type=owc_definition,
+            help="OWC definition, e.g. SWAT:0.7",
+        )
+        parser.add_argument(
+            "-d", "--delta", type=float, help="Delta Z dogleg restriction, e.g. 0.0165"
+        )
+        parser.add_argument(
+            "-w",
+            "--overwrite",
+            action="store_true",
+            help="Overwrite output files will overwrite existing files",
+        )
+        parser.add_argument(
+            "-r",
+            "--resinsight",
+            action="store_true",
+            help="ResInsight compatible output format instead of RMS",
+        )
+        parser.add_argument(
+            "--version",
+            action="version",
+            version="%(prog)s {version}".format(version=VERSION),
+        )
+
+        parser.add_argument(
+            "config",
+            type=snapwell_config_file,
+            help="The Snapwell configuration file, e.g. snap.sc",
+        )
+        return parser
+
+    def runner(self):
+        args = self.parse_args()
+        config = self.load_config(args)
+        return SnapwellRunner(
+            config,
+            self.load_grid_file(config),
+            self.load_restart_file(config),
+            self.load_init_file(config),
+            self.load_wellpaths(config),
+            args.resinsight,
+        )
+
+    def run(self):
+        fullstart = time()
+
+        runner = self.runner()
+
+        confstop = time()
+        conftime = round(confstop - fullstart, 2)
+        print("\n\nConfiguration completed in %s sec.\n" % str(conftime))
+
+        runner.main_loop()
+
+        fullstop = time()
+        fullsec = round(fullstop - fullstart, 2)
+        print("snapwell completed in %s seconds" % str(fullsec))
 
 
 def main():
-    try:
-        from web_log import LogAsync
-
-        LogAsync("snapwell", "launch")
-    except:
-        pass
-    if len(sys.argv) == 1:
-        exit_with_usage()
-    elif len(sys.argv) == 2:
-        if sys.argv[1] == "-v" or sys.argv[1] == "--version":
-            print("%s %s" % ("snapwell", VERSION))
-            exit(0)
-
-    parser = argparse.ArgumentParser(
-        description="Snapwell --- a wellpath optimization program."
-    )
-    parser.add_argument(
-        "-o",
-        "--output",
-        type=str,
-        help="Output folder.  In this folder, all the new wellpath files are written on the specified format.",
-    )
-    parser.add_argument(
-        "-z", "--owc_offset", type=float, help="OWC offset in meters, e.g. 0.5"
-    )
-    parser.add_argument(
-        "-f", "--owc_definition", type=str, help="OWC definition, e.g. SWAT:0.7"
-    )
-    parser.add_argument(
-        "-d", "--delta", type=float, help="Delta Z dogleg restriction, e.g. 0.0165"
-    )
-    parser.add_argument(
-        "-w",
-        "--overwrite",
-        action="store_true",
-        help="Overwrite output files will overwrite existing files",
-    )
-    parser.add_argument(
-        "-r",
-        "--resinsight",
-        action="store_true",
-        help="ResInsight compatible output format instead of RMS",
-    )
-    parser.add_argument(
-        "-v",
-        "--version",
-        action="store_true",
-        help="show snapwell version (%s) and exit" % VERSION,
-    )
-
-    parser.add_argument("config", help="The Snapwell configuration file, e.g. snap.sc")
-
-    args = parser.parse_args()
-
-    fullstart = time()
-
-    conf_file = get_conf_file(args.config)
-    snap_conf = get_snap_conf(conf_file)
-
-    if args.owc_offset:
-        snap_conf.setOwcOffset(args.owc_offset)
-    if args.output:
-        outpath = path.abspath(args.output)
-        if path.isfile(outpath):
-            exit_with_usage(
-                "Output path is an existing file.  Delete file, or choose a different output path."
-            )
-        if not path.exists(outpath):
-            makedirs(outpath)
-        snap_conf.setOutput(outpath)
-    if args.overwrite:
-        snap_conf.setOverwrite(args.overwrite)
-    if args.owc_definition:
-        d = _parseOwcDefinition(args.owc_definition)
-        if d:
-            snap_conf.setOwcDefinition(d)
-        else:
-            exit_with_usage("Incorrect usage.  Use as --owc_definition SWAT:0.07")
-    if args.owc_offset:
-        snap_conf.setOwcOffset(args.owc_offset)
-    if args.delta:
-        snap_conf.setDeltaZ(args.delta)
-
-    grid = load_grid_file(snap_conf)
-    restart = load_restart_file(snap_conf)
-    init = load_init_file(snap_conf)
-    wellpaths = load_wellpaths(snap_conf)
-    confstop = time()
-    conftime = round(confstop - fullstart, 2)
-    print("\n\nConfiguration completed in %s sec.\n" % str(conftime))
-
-    # This function loops through all wellpaths wp in wellpaths
-    main_loop(grid, restart, init, snap_conf, wellpaths, resinsight=args.resinsight)
-
-    fullstop = time()
-    fullsec = round(fullstop - fullstart, 2)
-    print("snapwell completed in %s seconds" % str(fullsec))
+    logging.info("Snapwell launched")
+    warnings.showwarning = warn_without_traceback
+    app = SnapwellApp(sys.argv)
+    app.run()
 
 
 if __name__ == "__main__":
---
+++ tests/test_snapwell_main.py
@@ -0,0 +1,121 @@
+from os import path
+
+import pytest
+import snapwell
+import snapwell.snapwell_main as swm
+
+
+def test_version(capsys):
+    app = swm.SnapwellApp(["snapwell", "--version"])
+    with pytest.raises(SystemExit) as e:
+        app.run()
+    assert e.value.code == 0
+    assert snapwell.__version__ in capsys.readouterr().out
+
+
+def test_run_missing(capsys, tmp_path):
+    missing_file = path.join(tmp_path, "__missing__")
+    app = swm.SnapwellApp(["snapwell", missing_file])
+    with pytest.raises(SystemExit) as e:
+        app.parse_args()
+    assert e.value.code == 2
+    assert "No such file" in capsys.readouterr().err
+
+
+def test_missing_config_grid(capsys, tmp_path):
+    config_file_path = path.join(tmp_path, "config.sc")
+
+    with open(config_file_path, "w") as config_file:
+        config_file.write("GRID\n")
+        config_file.write("RESTART a.UNRST\n")
+        config_file.write("WELLPATH b.w 2020\n")
+
+    app = swm.SnapwellApp(["snapwell", config_file_path])
+    with pytest.raises(SystemExit) as e:
+        app.load_config(app.parse_args())
+    assert e.value.code == 2
+    assert "Could not load GRID" in capsys.readouterr().err
+
+
+def test_missing_config_restart(capsys, tmp_path):
+    config_file_path = path.join(tmp_path, "config.sc")
+
+    with open(config_file_path, "w") as config_file:
+        config_file.write("GRID a.grid\n")
+        config_file.write("RESTART\n")
+        config_file.write("WELLPATH b.w 2020\n")
+
+    app = swm.SnapwellApp(["snapwell", config_file_path])
+    with pytest.raises(SystemExit) as e:
+        app.load_config(app.parse_args())
+    assert e.value.code == 2
+    assert "Could not load RESTART" in capsys.readouterr().err
+
+
+def write_config(
+    config_file_path,
+    grid_file="grid.EGRID",
+    restart_file="restart.UNRST",
+    well_files=[("well1.w", 2020)],
+):
+    with open(config_file_path, "w") as config_file:
+        config_file.write(f"GRID {grid_file}\n")
+        config_file.write(f"RESTART {restart_file}\n")
+        for wf in well_files:
+            config_file.write(f"WELLPATH {wf[0]} {wf[1]}\n")
+
+
+def test_config_sets_correct_paths(tmp_path):
+    config_file_path = path.join(tmp_path, "config.sc")
+
+    grid_file = "a.EGRID"
+    restart_file = "b.UNRST"
+    well_file = "c.w"
+    year = 2020
+
+    write_config(config_file_path, grid_file, restart_file, [(well_file, year)])
+
+    app = swm.SnapwellApp(["snapwell", config_file_path])
+    config = app.load_config(app.parse_args())
+
+    assert config.gridFile() == path.join(tmp_path, grid_file)
+    assert config.restartFile() == path.join(tmp_path, restart_file)
+    assert config.filename(0) == path.join(tmp_path, well_file)
+    assert config.date(0).year == year
+    assert not config.overwrite()
+
+
+def test_overwrite(tmp_path):
+    config_file_path = path.join(tmp_path, "config.sc")
+    write_config(config_file_path)
+
+    app = swm.SnapwellApp(["snapwell", config_file_path, "--overwrite"])
+    config = app.load_config(app.parse_args())
+    assert config.overwrite()
+
+    app2 = swm.SnapwellApp(["snapwell", config_file_path, "-w"])
+    config2 = app2.load_config(app2.parse_args())
+    assert config2.overwrite()
+
+
+test_data_path = path.join(path.dirname(__file__), "testdata", "snapwell")
+
+
+def test_runner_correct_wellpaths():
+    runner = swm.SnapwellApp(
+        ["snapwell", path.join(test_data_path, "test.sc")]
+    ).runner()
+    assert len(runner.wellpaths) == 1
+    wp = runner.wellpaths[0]
+    assert wp.welltype() == "A - B"
+    assert wp.version() == "2.0"
+    rows = list(wp.rows())
+    assert len(rows) == 4
+    assert all(len(r) == 6 for r in rows)
+
+
+def test_run_test_data_exits_with_zero():
+    test_data_path = path.join(path.dirname(__file__), "testdata", "snapwell")
+
+    swm.SnapwellApp(["snapwell", path.join(test_data_path, "test-depth.sc")]).run()
+    swm.SnapwellApp(["snapwell", path.join(test_data_path, "test.sc")]).run()
