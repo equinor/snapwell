2414d211e70466c12801476314e98fd11322b39e
Øyvind Eide <eide.oyvind87@gmail.com>
2020-10-14 10:09:04 +0200

    Fix install requirements

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setuptools.setup(
     name="snapwell",
     version="1.0.0",
     packages=["snapwell"],
-    requirements=["libecl", "equinor-libres"],
+    install_requires=["libecl", "equinor-libres"],
     entry_points={
         "console_scripts": [
             "snapwell=snapwell.snapwell_main:main",
