52948609eb92cb70cc9bdd6cdbae0e30d3df3cf8
Øyvind Eide <eide.oyvind87@gmail.com>
2020-10-14 14:46:13 +0200

    Add metadata to setup.py and add version

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
-import setuptools
+from pathlib import Path
+from setuptools import setup
 
-setuptools.setup(
+
+def get_long_description() -> str:
+    return Path("README.md").read_text(encoding="utf8")
+
+
+setup(
     name="snapwell",
-    version="1.0.0",
+    use_scm_version={"write_to": "snapwell/version.py"},
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
     packages=["snapwell"],
     install_requires=["libecl", "equinor-libres"],
     entry_points={
@@ -10,4 +18,21 @@ setuptools.setup(
             "snapwell=snapwell.snapwell_main:main",
         ]
     },
+    license="GPL-3.0",
+    platforms="any",
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Environment :: Plugins",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    setup_requires=["setuptools_scm"],
 )
